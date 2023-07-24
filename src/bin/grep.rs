fecdfc7177437228db76d900a0204a985743284a
fengzhongzhu1621 <jinyinqiao@gmail.com>
2023-07-24 23:05:02 +0800

    feat: add iterator demo

--- src/bin/grep.rs
+++ src/bin/grep.rs
@@ -1,19 +1,41 @@
 use std::env;
 use std::process;
+use std::thread;
+use std::time::Duration;
+use xrust::Cacher;
 use xrust::Config;
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_closure = Cacher::new(|num| {
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("intensity is {}", expensive_closure.value(intensity));
+        println!("intensity is {}", expensive_closure.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("random_number is {}", random_number);
+        } else {
+            println!("intensity is {}", expensive_closure.value(intensity));
+        }
+    }
+}
 
 fn main() {
     // 获得命令行参数，ascii字符
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // 参数复制给变量
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
+    // 执行业务逻辑
     if let Err(e) = xrust::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,23 @@ use std::error::Error;
 use std::fs;
 
 pub struct Config {
-    query: String,
-    filename: String,
-    case_sensitive: bool,
+    query: String,        // 查询参数
+    filename: String,     // 文件命名
+    case_sensitive: bool, // 是否区分大小写
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // 读取文件内容
     let contents = fs::read_to_string(config.filename)?;
 
+    // 根据查询字符串查询文件
     let results = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
     };
 
+    // 打印搜索到的文件行
     for line in results {
         println!("{}", line)
     }
@@ -26,13 +28,28 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    // 构造函数，返回Result类型
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        // let query = args[1].clone();
+        // let filename = args[2].clone();
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        // 判断环境变了是否设置，如果没有设置is_err() 返回 true
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        // 成功返回Ok枚举
         Ok(Config {
             query,
             filename,
@@ -41,16 +58,19 @@ impl Config {
     }
 }
 
+// 搜索字符串，区分大小写
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    //let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -64,4 +84,104 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     }
 
     results
+}
+
+pub struct Cacher<T>
+where
+    T: Fn(u32) -> u32, // 闭包
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    // 构造函数，参数是一个闭包
+    pub fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v, // 如果已缓冲，则直接返回
+            None => {
+                // 计算并存储缓冲
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|x| x.size == shoe_size).collect()
+}
+
+#[test]
+fn filter_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 12,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    assert_eq!(
+        in_my_size,
+        vec![Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },]
+    );
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
 }
