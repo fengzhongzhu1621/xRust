584fa49497b6d719bed851a1eaf50d24f2dc095e
fengzhongzhu1621 <jinyinqiao@gmail.com>
2023-08-07 23:16:11 +0800

    feat: add asyncwait/runtimes

--- asyncwait/src/lib.rs
+++ asyncwait/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod asyncio;
 pub mod future;
+pub mod runtimes;
 
 // 将路径 ./asyncio.rs导入到模块
 pub use asyncio::*;
 pub use future::*;
+pub use runtimes::*;
--- asyncwait/src/main.rs
+++ asyncwait/src/main.rs
@@ -6,5 +6,23 @@ fn main() {
     futures_lite_io();
 
     // 模拟 future 运行
-    timefuture_async()
+    timefuture_async();
+
+    // tokio 运行时
+    tokio_async();
+
+    // futures::executor 运行时
+    futures_async();
+
+    // 其它运行时
+    futures_lite_async();
+    async_std_demo();
+    smol_async();
+
+    // 等待task执行完
+    join();
+
+    // 执行select操作
+    select();
+    futures_select();
 }
