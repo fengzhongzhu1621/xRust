611d01c3db760538e162a8713c89b2f7c08f5787
fengzhongzhu1621 <jinyinqiao@gmail.com>
2023-10-13 10:22:44 +0800

    style: add comment

--- logger/src/messages.rs
+++ logger/src/messages.rs
@@ -1,10 +1,12 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 
+/// 消息显示开关
 static MESSAGES: AtomicBool = AtomicBool::new(false);
 static IGNORE_MESSAGES: AtomicBool = AtomicBool::new(false);
 static ERRORED: AtomicBool = AtomicBool::new(false);
 
 /// Like eprintln, but locks STDOUT to prevent interleaving lines.
+/// 打印日志到标准错误输出
 #[macro_export]
 macro_rules! eprintln_locked {
     // 参数可以出现 0 次或多次
@@ -25,9 +27,11 @@ macro_rules! eprintln_locked {
 }
 
 /// Emit a non-fatal error message, unless messages were disabled.
+/// 当日志开关打开时，打印日志到标准错误输出
 #[macro_export]
 macro_rules! message {
     ($($tt:tt)*) => {
+        //  crate:: 表示绝对路径
         if crate::messages::messages() {
             eprintln_locked!($($tt)*);
         }
@@ -36,6 +40,7 @@ macro_rules! message {
 
 /// Like message, but sets ripgrep's "errored" flag, which controls the exit
 /// status.
+/// 当日志开关打开时，打印日志到标准错误输出，且设置错误标识，用于判断程序是否退出
 #[macro_export]
 macro_rules! err_message {
     ($($tt:tt)*) => {
@@ -46,6 +51,7 @@ macro_rules! err_message {
 
 /// Emit a non-fatal ignore-related error message (like a parse error), unless
 /// ignore-messages were disabled.
+/// 当日志开关打开时且“忽略”日志开关打开，除了正常的日志外，还打印被标记为忽略状态的消息到标准错误输出
 #[macro_export]
 macro_rules! ignore_message {
     ($($tt:tt)*) => {
@@ -56,6 +62,7 @@ macro_rules! ignore_message {
 }
 
 /// Returns true if and only if messages should be shown.
+/// 返回消息开关
 pub fn messages() -> bool {
     MESSAGES.load(Ordering::SeqCst)
 }
