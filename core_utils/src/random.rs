a07d1833f7672d4f695d6b4f31df0e45ffb90429
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-03-20 00:39:45 +0800

    feat: change random.rs

--- core_utils/src/random.rs
+++ core_utils/src/random.rs
@@ -1,4 +1,6 @@
+use rand::seq::SliceRandom;
 use rand::Rng;
+use rand_core::{OsRng, RngCore};
 
 const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
 abcdefghijklmnopqrstuvwxyz\
@@ -14,4 +16,30 @@ pub fn random_string(len: usize) -> String {
         .collect();
 
     return password;
+}
+
+pub fn random_string_2(size: usize) -> String {
+    let mut rng = &mut rand::thread_rng();
+    String::from_utf8(
+        CHARSET.choose_multiple(&mut rng, size).cloned().collect(),
+    )
+    .unwrap()
+}
+
+/// 生成随机 iv
+pub fn generate_iv() -> [u8; 16] {
+    let mut rng = OsRng;
+    let mut bytes = [0u8; 16];
+    rng.fill_bytes(&mut bytes);
+
+    bytes
+}
+
+/// 生成随机 iv
+pub fn generate_iv_2() -> [u8; 16] {
+    let mut rng = OsRng::default();
+    let mut bytes = [0u8; 16];
+    rng.fill_bytes(&mut bytes);
+
+    bytes
 }
--- core_utils/tests/test_random.rs
+++ core_utils/tests/test_random.rs
@@ -5,3 +5,21 @@ fn test_random_string() {
     let s = random_string(32);
     assert_eq!(s.len(), 32);
 }
+
+#[test]
+fn test_random_string_2() {
+    let s = random_string_2(32);
+    assert_eq!(s.len(), 32);
+}
+
+#[test]
+fn test_generate_iv() {
+    let s = generate_iv();
+    assert_eq!(s.len(), 16);
+}
+
+#[test]
+fn test_generate_iv_2() {
+    let s = generate_iv_2();
+    assert_eq!(s.len(), 16);
+}
