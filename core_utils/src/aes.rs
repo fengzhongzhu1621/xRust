d58e27a913e6e297c30f389c00034c77470a024c
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-03-22 00:24:02 +0800

    feat: support bitmap encrpt

--- core_utils/src/aes.rs
+++ core_utils/src/aes.rs
@@ -2,6 +2,13 @@ use base64::{engine::general_purpose, Engine as _};
 use crypto::buffer::{BufferResult, ReadBuffer, WriteBuffer};
 use crypto::{aes, blockmodes, buffer, symmetriccipher};
 
+use std::mem;
+use windows::Win32::Graphics::Gdi::{BITMAPFILEHEADER, BITMAPINFOHEADER};
+
+// 计算位图的文件头长度和位图信息头长度
+const FILE_HEADER_LEN: usize = mem::size_of::<BITMAPFILEHEADER>();
+const INFO_HEADER_LEN: usize = mem::size_of::<BITMAPINFOHEADER>();
+
 pub fn aes128_cbc_encrypt(
     data: &[u8],
     key: &[u8],
@@ -92,4 +99,49 @@ pub fn aes128_base64_decrypt(
 ) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
     let data = general_purpose::STANDARD.decode(cipher_text).unwrap();
     aes128_cbc_decrypt(&data, &key, &iv)
+}
+
+pub fn aes128_encrypt_bmp(
+    key: &[u8],
+    iv: &[u8],
+    bmp_buffer: Vec<u8>,
+) -> Vec<u8> {
+    // 计算位图头长度
+    let header_len = FILE_HEADER_LEN + INFO_HEADER_LEN;
+
+    // 获得位图的像素数据
+    let bmp_header = &bmp_buffer[..header_len];
+    let bmp_pixel_data = &bmp_buffer[header_len..];
+
+    // 只加密像素部分，保留头部不变
+    let encrypt_bmp_pixel_data =
+        aes128_cbc_encrypt(bmp_pixel_data, &key, &iv).unwrap();
+
+    // 生成新的位图
+    let mut encrypt_bmp_buffer = Vec::new();
+    encrypt_bmp_buffer.extend_from_slice(bmp_header);
+    encrypt_bmp_buffer.extend_from_slice(&encrypt_bmp_pixel_data);
+
+    encrypt_bmp_buffer
+}
+
+pub fn aes128_decrypt_bmp(
+    key: &[u8],
+    iv: &[u8],
+    bmp_buffer: Vec<u8>,
+) -> Vec<u8> {
+    let header_len = FILE_HEADER_LEN + INFO_HEADER_LEN;
+    let bmp_header = &bmp_buffer[..header_len];
+    let bmp_pixel_data = &bmp_buffer[header_len..];
+
+    // 解密像素部分
+    let decrypt_bmp_pixel_data =
+        aes128_cbc_decrypt(bmp_pixel_data, &key, &iv).unwrap();
+
+    // 生成原位图
+    let mut decrypt_bmp_buffer = Vec::new();
+    decrypt_bmp_buffer.extend_from_slice(bmp_header);
+    decrypt_bmp_buffer.extend_from_slice(&decrypt_bmp_pixel_data);
+
+    decrypt_bmp_buffer
 }
--- core_utils/tests/test_aes.rs
+++ core_utils/tests/test_aes.rs
@@ -1,8 +1,10 @@
+use bmp_rust::bmp;
 use core_utils::aes::*;
 use core_utils::random::*;
+use std::{env, fs};
 
 #[test]
-fn test_aes128_base64_encrypt() {
+fn test_aes128_base64() {
     let key = get_random_key16();
     let iv = generate_iv();
 
@@ -15,4 +17,34 @@ fn test_aes128_base64_encrypt() {
     let plain_text_2 = String::from_utf8(output).unwrap();
 
     assert_eq!(plain_text, plain_text_2);
+}
+
+#[test]
+fn test_aes128_encrypt_bmp() {
+    let key = get_random_key16();
+    let iv = generate_iv();
+
+    // 加密
+    let path = env::current_dir().unwrap().join("tests/test-image.bmp");
+    let bmp_buffer = fs::read(path).unwrap();
+    let encrypt_bmp_buffer = aes128_encrypt_bmp(&key, &iv, bmp_buffer);
+    let encrypt_path =
+        env::current_dir().unwrap().join("tests/encrypted-test-image.bmp");
+    fs::write(encrypt_path.as_path(), encrypt_bmp_buffer).unwrap();
+
+    // 解密
+    let bmp_buffer = fs::read(encrypt_path).unwrap();
+    let decrypt_bmp_buffer = aes128_decrypt_bmp(&key, &iv, bmp_buffer);
+    let encrypt_path =
+        env::current_dir().unwrap().join("tests/decrypted-test-image.bmp");
+    fs::write(encrypt_path, decrypt_bmp_buffer).unwrap();
+
+    // 解密后的位图和原始问题简单比较
+    let bmp_from_file_origin = bmp::BMP::new_from_file("tests/test-image.bmp");
+    let bmp_from_file_decrypt =
+        bmp::BMP::new_from_file("tests/decrypted-test-image.bmp");
+    let diff_result =
+        bmp::BMP::diff(&bmp_from_file_origin, &bmp_from_file_decrypt).unwrap();
+    assert_eq!(diff_result.is_same_size(), true);
+    assert_eq!(diff_result.diff.len(), 0);
 }
