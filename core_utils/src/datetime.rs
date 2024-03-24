5b6bfef76153bdc7a63f34bf436f77cc07e3c637
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-03-24 23:00:15 +0800

    feat: add datetime func

--- core_utils/src/datetime.rs
+++ core_utils/src/datetime.rs
@@ -1,5 +1,9 @@
 use chrono::prelude::*;
+use std::time::SystemTime;
 
+const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
+/// 将当前时间转换为UTC时区的字符串格式
 pub fn format_datetime() -> String {
     let now = Utc::now();
     return now.format("%Y-%m-%d %H:%M:%S").to_string();
@@ -10,6 +14,15 @@ pub fn format_date() -> String {
     return now.format("%Y-%m-%d").to_string();
 }
 
+/// 将 SystemTime 转换为字符串格式
+pub fn format_system_time(st: SystemTime) -> String {
+    // 获得本机时间
+    let local_datetime: DateTime<Local> = st.clone().into();
+    // 将本机时间格式化为字符串
+    local_datetime.format(DATETIME_FORMAT).to_string()
+}
+
+/// 根据日期字符串获得是第几个星期
 pub fn get_weekday_index(date: &str) -> u32 {
     // 将日期字符串转换为日期对象
     let now = NaiveDate::parse_from_str(date, "%Y-%m-%d").unwrap();
@@ -17,4 +30,29 @@ pub fn get_weekday_index(date: &str) -> u32 {
     let day_index = now.ordinal();
 
     return (day_index / 7) + 1;
+}
+
+/// 将 SystemTime 转换为UNIX时间戳的秒表示
+pub fn to_seconds(st: SystemTime) -> i64 {
+    let local_datetime: DateTime<Local> = st.clone().into();
+    local_datetime.timestamp()
+}
+
+/// 将 SystemTime 转换为UNIX时间戳的毫秒表示
+pub fn to_mill_seconds(st: SystemTime) -> i64 {
+    let local_datetime: DateTime<Local> = st.clone().into();
+    local_datetime.timestamp_millis()
+}
+
+/// 获得当前时间戳
+pub fn now_to_seconds() -> i64 {
+    let now = Local::now();
+    now.timestamp()
+}
+
+/// 将当前时区的时间字符串转换为 SystenTime
+pub fn to_system_time(datetime_str: &str) -> SystemTime {
+    let no_timezone =
+        NaiveDateTime::parse_from_str(datetime_str, DATETIME_FORMAT).unwrap();
+    Local.from_local_datetime(&no_timezone).unwrap().into()
 }
--- core_utils/tests/test_datetime.rs
+++ core_utils/tests/test_datetime.rs
@@ -1,8 +1,9 @@
-use core_utils::datetime;
+use core_utils::datetime::*;
+use std::time::SystemTime;
 
 #[test]
 fn test_format_datetime() {
-    let now = datetime::format_datetime();
+    let now = format_datetime();
     let actual = now.len();
     let expect = 19;
 
@@ -12,7 +13,7 @@ fn test_format_datetime() {
 
 #[test]
 fn test_format_date() {
-    let now = datetime::format_date();
+    let now = format_date();
     let actual = now.len();
     let expect = 10;
 
@@ -22,8 +23,24 @@ fn test_format_date() {
 
 #[test]
 fn test_get_weekday_index() {
-    let now_str = "2023-01-08";
-    let actual = datetime::get_weekday_index(&now_str);
+    let now_str = "2024-01-08";
+    let actual = get_weekday_index(&now_str);
     let expect = 2;
     assert_eq!(actual, expect);
+}
+
+#[test]
+fn test_to_timestamp() {
+    let st = SystemTime::now();
+    println!("{:?} {:?}", st, to_seconds(st));
+
+    println!("{:?} {:?}", st, to_mill_seconds(st));
+    println!("{:?}", now_to_seconds());
+}
+
+#[test]
+fn test_to_system_time() {
+    let str = "2024-01-02 03:04:05";
+    let st = to_system_time(str);
+    assert_eq!(to_seconds(st), 1704135845);
 }
