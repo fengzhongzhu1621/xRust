16d8e6ed26f8180cf4229c9b5b535a4dfac94163
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-04-30 16:17:37 +0800

    test: add test_unicode_segmentation

---
+++ xtool/tests/test_unicode_segmentation.rs
@@ -0,0 +1,24 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+#[test]
+fn test_unicode_segmentation() {
+    let s = "a̐éö̲\r\n";
+    let g = s.graphemes(true).collect::<Vec<&str>>();
+    let b: &[_] = &["a̐", "é", "ö̲", "\r\n"];
+    assert_eq!(g, b);
+
+    let s = "The quick (\"brown\") fox can't jump 32.3 feet, right?";
+    let w = s.unicode_words().collect::<Vec<&str>>();
+    let b: &[_] = &[
+        "The", "quick", "brown", "fox", "can't", "jump", "32.3", "feet",
+        "right",
+    ];
+    assert_eq!(w, b);
+
+    let s = "The quick (\"brown\")  fox";
+    let w = s.split_word_bounds().collect::<Vec<&str>>();
+    let b: &[_] = &[
+        "The", " ", "quick", " ", "(", "\"", "brown", "\"", ")", "  ", "fox",
+    ];
+    assert_eq!(w, b);
+}
